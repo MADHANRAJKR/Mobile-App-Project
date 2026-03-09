d10cda677f591e4b7a631f5107f62c67391d4767
Madhan Raj K R <167062420+MADHANRAJKR@users.noreply.github.com>
2026-03-09 18:24:15 +0530

    Update DetailScreen.js

--- Screens/DetailScreen.js
+++ Screens/DetailScreen.js
@@ -1,1 +1,20 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
 
+export default function DetailScreen({ route }) {
+  const { item } = route.params;
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Item Detail</Text>
+
+      <Text>Name: {item.name}</Text>
+      <Text>Description: {item.description}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: "center", alignItems: "center" },
+  title: { fontSize: 22, marginBottom: 20 },
+});
